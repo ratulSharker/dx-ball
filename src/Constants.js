73bd008a0c51e16e0d8d1e0b71d384b28bff60df
Ratul Sharker <Sharker.ratul.08@gmail.com>
2020-07-14 14:47:03 +0600

    issue #1
    1. `eslint` added for code linting.

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,5 @@
+const process = require("process")
+
 module.exports.Constants = {
-    port : process.env.PORT
-};
+	port : process.env.PORT
+}
--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const { Constants } = require('./Constants')
+/* global __dirname */
+
+const express = require("express")
+const { Constants } = require("./Constants")
 const path = require("path")
 
 const app = express()
 
-app.use('/', express.static(path.join(__dirname, 'game')))
-app.get('/api', (req, res) => res.send('Hello World!'))
+
+app.use("/", express.static(path.join(__dirname, "game")))
+
+
+app.get("/api", (req, res) => res.send("Hello World!"))
 
 app.listen(Constants.port, () => console.log(`Example app listening at http://localhost:${Constants.port}`))
